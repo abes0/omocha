1523633336483cb10d9e0b22a5af2b077042ec53
Shinya Abe <s-abe@details.co.jp>
2026-08-13 08:54:49 +0900

    add qr size

--- src/app/qr-check/print/page.tsx
+++ src/app/qr-check/print/page.tsx
@@ -50,7 +50,7 @@ export default function PrintPage() {
         <div className="flex w-full flex-1 flex-col items-center justify-center rounded-3xl border border-slate-200 bg-white p-6 text-slate-900 shadow-xl print:min-h-dvh print:w-full print:flex-none print:rounded-none print:border-none print:shadow-none">
           {svg ? (
             <div
-              className="aspect-square w-full max-w-xs [&_svg]:h-full [&_svg]:w-full print:w-[3cm] print:max-w-none"
+              className="aspect-square w-full max-w-xs [&_svg]:h-full [&_svg]:w-full print:w-[2cm] print:max-w-none"
               dangerouslySetInnerHTML={{ __html: svg }}
             />
           ) : (
